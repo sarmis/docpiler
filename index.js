c3523f19d2ac2ae96fca43c69a3f8b12cd561935
Thomas Sarmis <thomas.sarmis@gmail.com>
2018-03-07 07:07:08 +0200

    .

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var doc = Docpiler('./examples/static-site');
 
 doc.do('find', {})
 doc.do('import')
+doc.do( (file, state, opts) => {file.data += ' __I told you so!!!__ '} )
 doc.do('parse-frontmatter')
 doc.do('parse-markdown')
+doc.do( (file, state, opts) => {file.data.body += '<u>Yeap</u>'} )
+//doc.do('render-pug')
 doc.do(require('./modules/docpiler-render-pug'))
 doc.do('export')
 
